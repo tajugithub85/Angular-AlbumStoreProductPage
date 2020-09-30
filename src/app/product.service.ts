0f5e3ce856a7225294b2c961f69660683e0c9461
tajugithub85clear <62436203+tajugithub85@users.noreply.github.com>
2020-09-30 21:29:43 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
+import { Http , Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
 
 
 @Injectable()
 export class ProductService {
-
-  constructor(private http: HttpClient) { }
-
-}
+  private _producturl = 'app/products.json';
+  constructor(private _http: Http) { }
+   
+   
+}     
